98a8e3b82c3e67d35eb89f28b0cc9173d2844091
TranVoGiaHuy <gabokan02564@gmail.com>
2026-02-02 10:42:36 +0700

    feat: init product, ingredient, formula repositories

---
+++ CentralKitchen/src/main/java/com/groupSWP/centralkitchenplatform/repositories/FormulaRepository.java
@@ -0,0 +1,10 @@
+package com.groupSWP.centralkitchenplatform.repositories;
+
+import com.groupSWP.centralkitchenplatform.entities.kitchen.Formula;
+import com.groupSWP.centralkitchenplatform.entities.kitchen.FormulaKey;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FormulaRepository extends JpaRepository<Formula, FormulaKey> {
+}
