52bf60105a5da4c2bb6145ed3a2093500dfdbd1b
jonhhh231 <tnguyndatnguyen251@gmail.com>
2026-02-28 22:21:55 +0700

    add enum state

---
+++ CentralKitchen/src/main/java/com/groupSWP/centralkitchenplatform/entities/logistic/ShipmentType.java
@@ -0,0 +1,6 @@
+package com.groupSWP.centralkitchenplatform.entities.logistic;
+
+public enum ShipmentType {
+    STANDARD,      // Đơn giao hàng bình thường
+    REPLACEMENT    // Đơn giao bù (xử lý sự cố)
+}
