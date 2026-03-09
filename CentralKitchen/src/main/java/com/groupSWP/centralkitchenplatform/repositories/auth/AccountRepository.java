c474eb3ff80729ebd4497dfe6eba04163af8a961
jonhhh231 <tnguyndatnguyen251@gmail.com>
2026-03-09 13:08:55 +0700

    list account by name

--- CentralKitchen/src/main/java/com/groupSWP/centralkitchenplatform/repositories/auth/AccountRepository.java
+++ CentralKitchen/src/main/java/com/groupSWP/centralkitchenplatform/repositories/auth/AccountRepository.java
@@ -15,4 +15,8 @@ public interface AccountRepository extends JpaRepository<Account, UUID> {
     List<Account> findAllExcludingAdmin();
     @Query("SELECT a FROM Account a LEFT JOIN FETCH a.systemUser WHERE a.role != 'ADMIN' AND a.isActive = :isActive")
     List<Account> findByIsActiveExcludingAdmin(@Param("isActive") boolean isActive);
+
+    // Tìm kiếm theo tên gần đúng (không phân biệt hoa thường) và loại trừ ADMIN
+    @Query("SELECT a FROM Account a JOIN a.systemUser u WHERE LOWER(u.fullName) LIKE LOWER(CONCAT('%', :keyword, '%')) AND a.role != 'ADMIN'")
+    List<Account> searchByFullNameExcludingAdmin(@Param("keyword") String keyword);
 }
